7ee7921eb81124dfe3f44f634b53d113c6dd84e9
Matuxa1503 <muravskiy.03@mail.ru>
2023-11-04 20:24:24 +0300

    added all files

--- plants/index.js
+++ plants/index.js
@@ -26,45 +26,76 @@ window.addEventListener('click', function(e) {
 });
 
 
+
+
+
+
+
+
 // Blur
-const gardensBtn1 = document.getElementById("gardens__btn1");
-const lawnBtn2 = document.getElementById("lawn__btn2");
-const plantingBtn3 = document.getElementById("planting__btn3");
+// const gardensBtn1 = document.getElementById("gardens__btn1");
+// const lawnBtn2 = document.getElementById("lawn__btn2");
+// const plantingBtn3 = document.getElementById("planting__btn3");
 
-const gardens = document.querySelectorAll(".garden");
-const lawn = document.querySelectorAll(".lawn");
-const planting = document.querySelectorAll(".planting");
-
-
-gardens.forEach(item => gardensBtn1.addEventListener("click", function(e) {
-	item.classList.toggle("gardenBlur");
-}));
-
-lawn.forEach(item => lawnBtn2.addEventListener("click", function(e) {
-	item.classList.toggle("lawnBlur");
-}));
-
-planting.forEach(item => plantingBtn3.addEventListener("click", function(e) {
-	item.classList.toggle("plantingBlur");
-}))
+// const gardens = document.querySelectorAll(".garden");
+// const lawn = document.querySelectorAll(".lawn");
+// const planting = document.querySelectorAll(".planting");
 
 
-if (gardensBtn1) {
-	gardensBtn1.addEventListener("click", function(e) {
-		gardensBtn1.classList.toggle("activeBtn1");
-	});
-};
+// gardens.forEach(item => gardensBtn1.addEventListener("click", function(e) {
+// 	item.classList.toggle("gardenBlur");
+// }));
 
-if (lawnBtn2) {
-	lawnBtn2.addEventListener("click", function(e) {
-		lawnBtn2.classList.toggle("activeBtn2");
-	});
-};
+// lawn.forEach(item => lawnBtn2.addEventListener("click", function(e) {
+// 	item.classList.toggle("lawnBlur");
+// }));
 
-if (plantingBtn3) {
-	plantingBtn3.addEventListener("click", function(e) {
-		plantingBtn3.classList.toggle("activeBtn3");
-	});
-};
+// planting.forEach(item => plantingBtn3.addEventListener("click", function(e) {
+// 	item.classList.toggle("plantingBlur");
+// }))
+
+
+// if (gardensBtn1) {
+// 	gardensBtn1.addEventListener("click", function(e) {
+// 		gardensBtn1.classList.toggle("activeBtn1");
+// 	});
+// };
+
+// if (lawnBtn2) {
+// 	lawnBtn2.addEventListener("click", function(e) {
+// 		lawnBtn2.classList.toggle("activeBtn2");
+// 	});
+// };
+
+// if (plantingBtn3) {
+// 	plantingBtn3.addEventListener("click", function(e) {
+// 		plantingBtn3.classList.toggle("activeBtn3");
+// 	});
+// };
+
+
+// // Accordion
+// document.querySelectorAll(".accordion__item").forEach((el) => {
+// 	el.addEventListener("click", () => {
+// 		let content = el.nextElementSibling;
+// 		if (content.style.maxHeight) {
+// 			document.querySelectorAll(".accordion__item-active").forEach((el) => el.style.maxHeight = null)
+// 		} else {
+// 			document.querySelectorAll(".accordion__item-active").forEach((el) => el.style.maxHeight = null)
+// 			content.style.maxHeight = 108 + 'px';
+// 		}
+// 	});
+// });
+
+// document.querySelectorAll(".accordion__item").forEach((item) => {
+// 	item.addEventListener("click", () => {
+// 		item.classList.toggle("open");
+// 		const rotateIcon = document.querySelectorAll(".accordion__img");
+// 		rotateIcon.forEach(x => x.classList.toggle("rotate"));
+// 	});
+// });
+
+
+
 
 
